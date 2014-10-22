c111b3be50bc010fae672ba79c5dd96a7ad017cc
Jeff Taggart <jeff@jetaggart.com>
2014-10-21 19:41:38 -0600

    User can follow from profile

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
 describe "Viewing other users" do
-  it "shows another users profile" do
+  before do
     other_user = create_user(:first_name     => "John",
                              :last_name      => "Jankins",
                              :type_of_ranter => User.type_of_ranters[:daily],
                              :bio            => "This is the users bio")
 
-    rant = create_rant(:title  => "This is a rant title",
-                       :body   => "This is a rant body",
-                       :author => other_user)
+    @rant = create_rant(:title  => "This is a rant title",
+                        :body   => "This is a rant body",
+                        :author => other_user)
 
     login_user(create_user(:username => "psylinse"))
+  end
 
+  it "shows another users profile" do
     click_on "John Jankins"
 
     expect(page).to have_content("John Jankins")
@@ -22,9 +24,21 @@ describe "Viewing other users" do
     expect(page).to have_content("This is a rant title")
     expect(page).to have_content("This is a rant body")
 
-    visit rant_path(rant)
+    visit rant_path(@rant)
 
     click_on "John Jankins"
+
+    expect(page).to have_content("John Jankins")
+  end
+
+  it "allows a user to follow another user from their profile" do
+    click_on "John Jankins"
+
+    within("section", :text => "John Jankins") do
+      click_on "Follow"
+    end
+
+    click_on "Following"
 
     expect(page).to have_content("John Jankins")
   end
