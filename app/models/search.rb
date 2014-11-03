ea324cc31b24b14778f2458322206bc5062172d2
Jeff Taggart <jeff@jetaggart.com>
2014-11-03 13:03:17 -0700

    Search by first name

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,7 +6,25 @@ class Search
   def results
     return [] if query.blank?
 
-    sql_query = "%#{query}%"
-    Rant.joins(:author).where("users.last_name like ?", query)
+    Rant.joins(:author).where(where_statement, *bind_variables)
   end
+
+  private
+
+  def terms_to_search
+    ["first_name", "last_name"]
+  end
+
+  def bind_variable
+    "%#{query}%"
+  end
+
+  def bind_variables
+    terms_to_search.length.times.map { bind_variable }
+  end
+
+  def where_statement
+    terms_to_search.map { |t| "users.#{t} like ?" }.join(" OR ")
+  end
+
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 describe "A user searching for rants" do
   def search(query)
-    user = create_user(:last_name => "Taggart")
+    user = create_user(:first_name => "Jeff",
+                       :last_name => "Taggart")
 
     create_rant(:author => user, :title => "Find me!")
     create_rant(:title => "Don't find me")
@@ -24,6 +25,13 @@ describe "A user searching for rants" do
 
   it "allows a user to search by last name" do
     search("Taggart")
+
+    expect(page).to have_content("Find me!")
+    expect(page).to have_no_content("Don't find me")
+  end
+
+  it "allows a user to search by first name" do
+    search("Jeff")
 
     expect(page).to have_content("Find me!")
     expect(page).to have_no_content("Don't find me")
